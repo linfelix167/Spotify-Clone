ce337a4aa0d5f62d5e72abdac249de52e3a7821d
Felix Lin <swift.python.geeks@gmail.com>
2019-06-05 22:28:57 -0700

    RecyclerView setup

--- app/src/main/java/com/codingwithmitch/audiostreamer/ui/HomeFragment.java
+++ app/src/main/java/com/codingwithmitch/audiostreamer/ui/HomeFragment.java
@@ -2,24 +2,34 @@ package com.codingwithmitch.audiostreamer.ui;
 
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.codingwithmitch.audiostreamer.R;
+import com.codingwithmitch.audiostreamer.adapters.HomeRecyclerAdapter;
+
+import java.util.ArrayList;
 
 
-public class HomeFragment extends Fragment
+public class HomeFragment extends Fragment implements HomeRecyclerAdapter.IHomeSelector
 {
 
     private static final String TAG = "HomeFragment";
 
 
     // UI Components
+    private RecyclerView mRecyclerView;
 
 
     // Vars
+    private HomeRecyclerAdapter mAdapter;
+    private ArrayList<String> mCategories = new ArrayList<>();
 
 
     @Override
@@ -29,6 +39,24 @@ public class HomeFragment extends Fragment
     }
 
 
+    @Override
+    public void onCategorySelected(int position) {
+
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        initRecyclerView(view);
+    }
+
+    private void initRecyclerView(View view) {
+        if (mRecyclerView == null) {
+            mRecyclerView = view.findViewById(R.id.recycler_view);
+            mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+            mAdapter = new HomeRecyclerAdapter(mCategories, getActivity(), this);
+            mRecyclerView.setAdapter(mAdapter);
+        }
+    }
 }
 
 
