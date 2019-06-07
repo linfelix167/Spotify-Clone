561d63d5d90dbbbeb783d7bad6d15f17489df8c4
Felix Lin <swift.python.geeks@gmail.com>
2019-06-06 22:12:02 -0700

    Retrieve Firestore Data in HomeFragment, Improving the Query Experience in HomeFragment, RecyclerView Setup for CategoryFragment

--- app/src/main/java/com/codingwithmitch/audiostreamer/ui/CategoryFragment.java
+++ app/src/main/java/com/codingwithmitch/audiostreamer/ui/CategoryFragment.java
@@ -1,28 +1,55 @@
 package com.codingwithmitch.audiostreamer.ui;
 
 
+import android.content.Context;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.codingwithmitch.audiostreamer.R;
+import com.codingwithmitch.audiostreamer.adapters.CategoryRecyclerAdapter;
+import com.codingwithmitch.audiostreamer.adapters.HomeRecyclerAdapter;
+import com.codingwithmitch.audiostreamer.model.Artist;
+
+import java.util.ArrayList;
 
 
-
-public class CategoryFragment extends Fragment
-{
+public class CategoryFragment extends Fragment implements CategoryRecyclerAdapter.ICategorySelector {
 
     private static final String TAG = "CategoryFragment";
 
 
     // UI Components
+    private RecyclerView mRecyclerView;
 
 
     // Vars
+    private CategoryRecyclerAdapter mAdapter;
+    private ArrayList<Artist> mArtists = new ArrayList<>();
+    private IMainActivity mIMainActivity;
+    private String mSelectedCategory;
 
+    public static CategoryFragment newInstance(String category) {
+        CategoryFragment categoryFragment = new CategoryFragment();
+        Bundle args = new Bundle();
+        args.putString("category", category);
+        categoryFragment.setArguments(args);
+        return categoryFragment;
+    }
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mSelectedCategory = getArguments().getString("category");
+        }
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -30,8 +57,30 @@ public class CategoryFragment extends Fragment
         return inflater.inflate(R.layout.fragment_home, container, false);
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        initRecyclerView(view);
+    }
 
+    private void initRecyclerView(View view) {
+        if (mRecyclerView == null) {
+            mRecyclerView = view.findViewById(R.id.recycler_view);
+            mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+            mAdapter = new CategoryRecyclerAdapter(getActivity(), mArtists, this);
+            mRecyclerView.setAdapter(mAdapter);
+        }
+    }
 
+    @Override
+    public void onArtistSelected(int position) {
+
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        mIMainActivity = (IMainActivity) getActivity();
+    }
 }
 
 
--- app/src/main/java/com/codingwithmitch/audiostreamer/ui/HomeFragment.java
+++ app/src/main/java/com/codingwithmitch/audiostreamer/ui/HomeFragment.java
@@ -1,20 +1,28 @@
 package com.codingwithmitch.audiostreamer.ui;
 
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.codingwithmitch.audiostreamer.R;
 import com.codingwithmitch.audiostreamer.adapters.HomeRecyclerAdapter;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 
 public class HomeFragment extends Fragment implements HomeRecyclerAdapter.IHomeSelector
@@ -30,7 +38,11 @@ public class HomeFragment extends Fragment implements HomeRecyclerAdapter.IHomeS
     // Vars
     private HomeRecyclerAdapter mAdapter;
     private ArrayList<String> mCategories = new ArrayList<>();
+    private IMainActivity mIMainActivity;
 
+    public static HomeFragment newInstance() {
+        return new HomeFragment();
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -49,13 +61,47 @@ public class HomeFragment extends Fragment implements HomeRecyclerAdapter.IHomeS
         initRecyclerView(view);
     }
 
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        mIMainActivity = (IMainActivity) getActivity();
+    }
+
     private void initRecyclerView(View view) {
         if (mRecyclerView == null) {
             mRecyclerView = view.findViewById(R.id.recycler_view);
             mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
             mAdapter = new HomeRecyclerAdapter(mCategories, getActivity(), this);
             mRecyclerView.setAdapter(mAdapter);
+            retrieveCategories();
         }
+    }
+
+    private void retrieveCategories() {
+        mIMainActivity.showProgressBar();
+
+        FirebaseFirestore firestore = FirebaseFirestore.getInstance();
+
+        DocumentReference ref = firestore
+                .collection(getString(R.string.collection_audio))
+                .document(getString(R.string.document_categories));
+        ref.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot doc = task.getResult();
+                    Log.d(TAG, "onComplete: " + doc);
+                    HashMap<String, String> categoriesMap = (HashMap)doc.getData().get("categories");
+                    mCategories.addAll(categoriesMap.keySet());
+                }
+                updateDataSet();
+            }
+        });
+    }
+
+    private void updateDataSet() {
+        mIMainActivity.hideProgressBar();
+        mAdapter.notifyDataSetChanged();
     }
 }
 
